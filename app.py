7859ccebaf9cdd3f7c45543d259e3cb25ed05fa8
UpdaterCodeHub <asifuzzaman.lasker@yahoo.in>
2022-07-03 10:48:20 +0530

    CI CD pipeline established app working properly

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app=Flask(__name__)
 @app.route("/", methods=['GET','POST'])
 
 def index():
-    return "Start machine learning Porjenct"
+    return "CI CD pipleline has been established"
 
 if __name__=="__main__":
     app.run(debug=True)
